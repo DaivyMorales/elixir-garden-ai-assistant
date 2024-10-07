effcfd428e0af9bde80cca58afe00dac30abadaa
DaivyMorales <daivymorales7705@gmail.com>
2024-10-07 11:45:13 -0500

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Public_Sans } from "next/font/google";
+import Head from "next/head";
 
 import { api } from "@/utils/api";
 
@@ -15,11 +16,28 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
+    <>
+      <Head>
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WGQXFB6P');`}
+        </script>
+      </Head>
+      <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGQXFB6P"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
+        </noscript>
+        <SessionProvider session={session}>
+          <main className={inter.className}>
+            <Component {...pageProps} />
+          </main>
+        </SessionProvider>
+      </body>
+    </>
   );
 };
 
