4408641ebb9962ccee81d07e9731dd27d6032a00
DaivyMorales <daivymorales7705@gmail.com>
2024-11-19 10:48:07 -0500

    fix: quotes

--- src/components/FindResult.tsx
+++ src/components/FindResult.tsx
@@ -3,15 +3,15 @@ import { FaBottleDroplet } from "react-icons/fa6";
 
 function FindResult() {
   return (
-    <div className="z-10 flex max-w-[600px] text-white flex-col gap-4">
+    <div className="z-10 flex max-w-[600px] flex-col gap-4 text-white">
       <div>
-        <h2>Perfiles similares a "Invictus"</h2>
+        <h2>Perfiles similares a &quot;Invictus&quot;</h2>
         <p className="text-xs font-medium text-neutral-400">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia,
           facere.
         </p>
       </div>
-      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg p-4 text-[13px] font-medium backdrop-blur-sm bg-white/5">
+      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
         <div className="flex items-start justify-start gap-2">
           <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
             <span className="font-black text-black">1</span>
@@ -28,7 +28,7 @@ function FindResult() {
           </div>
         </div>
       </div>
-      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white p-4 text-[13px] font-medium backdrop-blur-sm bg-white/5">
+      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
         <div className="flex items-start justify-start gap-2">
           <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
             {" "}
@@ -46,7 +46,7 @@ function FindResult() {
           </div>
         </div>
       </div>
-      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg backdrop-blur-sm bg-white/5  p-4 text-[13px] font-medium">
+      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
         <div className="flex items-start justify-start gap-2">
           <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
             {" "}
