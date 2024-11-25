96a27f3105111228b519206a0411e1b238b79884
DaivyMorales <daivymorales7705@gmail.com>
2024-11-25 09:40:42 -0500

    feat: OpenAi response in the Front-end

--- src/components/FindResult.tsx
+++ src/components/FindResult.tsx
@@ -1,69 +1,31 @@
 import React from "react";
-import { FaBottleDroplet } from "react-icons/fa6";
 
-function FindResult() {
+function FindResult({ data, name }: { data: any; name: string }) {
   return (
     <div className="z-10 flex max-w-[600px] flex-col gap-4 text-white">
-      <div>
-        <h2>Perfiles similares a &quot;Invictus&quot;</h2>
+      <div className="z-10">
+        <h2 className="text-neutral-400">Perfiles similares a <span className="text-white font-bold">&quot;{name}&quot;</span></h2>
         <p className="text-xs font-medium text-neutral-400">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia,
-          facere.
+          Estos son los siguientes perfumes que mas favorecen a el perfume solicitado:
         </p>
       </div>
-      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
-        <div className="flex items-start justify-start gap-2">
-          <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
-            <span className="font-black text-black">1</span>
-          </div>
-          <div>
-            Invictus{" "}
-            <span className="rounded-lg bg-neutral-500 px-2 font-mono text-xs text-white">
-              M1
-            </span>
-            <p className="text-xs text-neutral-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis sit vitae id. Quidem, commodi. Enim.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
-        <div className="flex items-start justify-start gap-2">
-          <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
-            {" "}
-            <span className="font-black text-black">2</span>
-          </div>
-          <div>
-            Invictus{" "}
-            <span className="rounded-lg bg-neutral-500 px-2 font-mono text-xs text-white">
-              M1
-            </span>
-            <p className="text-xs text-neutral-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis sit vitae id. Quidem, commodi. Enim.
-            </p>
+
+      {data.map((perfum: any, index: number) => (
+        <div key={perfum.id} className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
+          <div className="flex items-start justify-start gap-2">
+            <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
+              <span className="font-black text-black">{index + 1}</span>
+            </div>
+            <div>
+              {perfum.name}{" "}
+              <span className="rounded-lg bg-neutral-500 px-2 font-mono text-xs text-white">
+                {perfum.id}
+              </span>
+              <p className="text-xs text-neutral-400">{perfum.reason}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="z-10 flex max-w-[600px] flex-col items-start justify-start gap-2 rounded-lg bg-white/5 p-4 text-[13px] font-medium backdrop-blur-sm">
-        <div className="flex items-start justify-start gap-2">
-          <div className="rounded-lg border-[1px] border-green-500 bg-green-400 px-3 py-1.5">
-            {" "}
-            <span className="font-black text-black">3</span>
-          </div>
-          <div>
-            One Million Men{" "}
-            <span className="rounded-lg bg-neutral-500 px-2 font-mono text-xs text-white">
-              H22
-            </span>
-            <p className="text-xs text-neutral-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis sit vitae id. Quidem, commodi. Enim.
-            </p>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
---
+++ src/pages/api/openai/find.ts
@@ -0,0 +1,95 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { name } = req.body;
+
+  const newPrompt = `Eres un asistente experto en perfumes. Mi cliente esta buscando el siguiente perfume: ${name}, pero no contamos con este en nuestro stock, necesito que me des los perfumes que mas se parezcan en olor a los siguientes perfumes que tenemos en stock:  [
+                { name: "Amber Oud Gold", id: "1U", genre: "UNISEX" },
+                { name: "Cloud", id: "1M", genre: "MUJER" },
+                { name: "Fantasy", id: "2M", genre: "MUJER" },
+                { name: "Burburry", id: "3M", genre: "MUJER" },
+                { name: "Omnia Coral", id: "4M", genre: "MUJER" },
+                { name: "King", id: "1H", genre: "HOMBRE" },
+                { name: "Ck One", id: "2U", genre: "UNISEX" },
+                { name: "Carolina Herrera", id: "5M", genre: "MUJER" },
+                { name: "212 Vip Rose", id: "6M", genre: "MUJER" },
+                { name: "212 Vip", id: "2H", genre: "HOMBRE" },
+                { name: "9 Pm", id: "3H", genre: "HOMBRE" },
+                { name: "Good Girl", id: "7M", genre: "MUJER" },
+                { name: "Chance", id: "8M", genre: "MUJER" },
+                { name: "212 Vip Black", id: "4H", genre: "HOMBRE" },
+                { name: "Bad Boy", id: "5H", genre: "HOMBRE" },
+                { name: "Sauvage", id: "6H", genre: "HOMBRE" },
+                { name: "Aventus", id: "8H", genre: "HOMBRE" },
+                { name: "Zero Plus", id: "9H", genre: "HOMBRE" },
+                { name: "Adrenaline Night", id: "10H", genre: "HOMBRE" },
+                { name: "Acqua Di Gio", id: "11H", genre: "HOMBRE" },
+                { name: "Issey Miyake", id: "12H", genre: "HOMBRE" },
+                { name: "Meow", id: "11M", genre: "MUJER" },
+                { name: "Bff Kim Kardashian", id: "12M", genre: "MUJER" },
+                { name: "Lacoste L.12.12 Red", id: "13H", genre: "HOMBRE" },
+                { name: "Lacoste L.12.12 White Men", id: "14H", genre: "HOMBRE" },
+                { name: "La Vie Est Belle", id: "13M", genre: "MUJER" },
+                { name: "Yara", id: "14M", genre: "MUJER" },
+                { name: "Starwalker", id: "15H", genre: "HOMBRE" },
+                { name: "Nautica", id: "16H", genre: "HOMBRE" },
+                { name: "Invictus", id: "17H", genre: "HOMBRE" },
+                { name: "Olympea", id: "15M", genre: "MUJER" },
+                { name: "Paris Hilton", id: "16M", genre: "MUJER" },
+                { name: "1 Million Men", id: "18H", genre: "HOMBRE" },
+                { name: "Tommy Girl", id: "19M", genre: "MUJER" },
+                { name: "Eros", id: "20M", genre: "MUJER" },
+                { name: "Tommy", id: "20H", genre: "HOMBRE" },
+                { name: "Coconut Passion", id: "22M", genre: "MUJER" },
+                { name: "Eros", id: "21H", genre: "HOMBRE" },
+                { name: "Amber Rouge", id: "3U", genre: "UNISEX" },
+                { name: "Club De Nuit Intense", id: "22H", genre: "HOMBRE" },
+                { name: "Khamra", id: "4U", genre: "UNISEX" },
+                { name: "Cloud Pink", id: "23M", genre: "MUJER" },
+                { name: "Asad", id: "23H", genre: "HOMBRE" },
+                { name: "Lataffa Sublime", id: "5U", genre: "UNISEX" },
+                { name: "Scandal", id: "24H", genre: "HOMBRE" },
+                { name: "360°", id: "24M", genre: "MUJER" },
+                { name: "Santal 33", id: "6U", genre: "UNISEX" },
+                { name: "Light Blue", id: "25M", genre: "MUJER" },
+              ]; 
+  **Formato de respuesta JSON**:
+  [
+    {
+      "id": "Id del perfume en stock", 
+      "name": "Nombre del perfume en stock",
+      "reason": "Por qué este perfume en stock es lo mas parecido a ${name} que el cliente busca"
+    }
+  ]
+
+
+  
+  Solo devuelve el JSON, no incluyas texto adicional.`;
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4",
+      messages: [{ role: "system", content: newPrompt }],
+      temperature: 0.7,
+      max_tokens: 500,
+    });
+
+    const result = response.choices[0]?.message?.content;
+
+    if (typeof result === "string") {
+      const recommendations = JSON.parse(result);
+      res.status(200).json(recommendations);
+    } else {
+      throw new Error("Invalid OpenAI response");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Failed to process OpenAI request" });
+  }
+}
